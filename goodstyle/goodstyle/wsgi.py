9b7256ea3fe44c7b5f73f6af28eb33a48286bd18
zaphodcat <zaphodcat@gmail.com>
2016-09-03 19:13:19 +1000

    mezzanne add

--- goodstyle/goodstyle/wsgi.py
+++ goodstyle/goodstyle/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "goodstyle.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("goodstyle"))
 
 application = get_wsgi_application()
